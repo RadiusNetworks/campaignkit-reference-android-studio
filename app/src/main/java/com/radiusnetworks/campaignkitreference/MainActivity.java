5d41625339e86640cea69b4b7d664ef4ff00c56b
Matt Tyler <matt.writes.code@gmail.com>
2014-10-15 16:07:53 -0700

    Working with CampaignKit 0.0.3

--- app/src/main/java/com/radiusnetworks/campaignkitreference/MainActivity.java
+++ app/src/main/java/com/radiusnetworks/campaignkitreference/MainActivity.java
@@ -3,8 +3,12 @@ package com.radiusnetworks.campaignkitreference;
 import java.util.HashMap;
 import java.util.Map;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.TableRow;
 
@@ -17,6 +21,8 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.radiusnetworks.campaignkitreference.DetailActivity;
 import com.radiusnetworks.campaignkitreference.MyApplication;
 import com.radiusnetworks.campaignkitreference.R;
@@ -42,7 +48,6 @@ public class MainActivity extends Activity {
 	private Context _context;
 
 	public static ArrayAdapter<String> listAdapter;
-	static final String RADIUS_UUID = "842AF9C4-08F5-11E3-9282-F23C91AEC05E";
 
 
 	@Override
@@ -57,7 +62,8 @@ public class MainActivity extends Activity {
 		_context = this;
 		setContentView(R.layout.activity_main);
 
-		verifyBluetooth();
+        verifyBluetooth();
+        googlePlayServicesConnected();
 		
 		findViewById(R.id.campaignsButton).setOnClickListener(new OnClickListener() {
 			
@@ -110,16 +116,21 @@ public class MainActivity extends Activity {
 				builder.setTitle("Bluetooth not enabled");			
 				builder.setMessage("Please enable bluetooth in settings and restart this application.");
 				builder.setPositiveButton(android.R.string.ok, null);
-				builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
-					@Override
-					public void onDismiss(DialogInterface dialog) {
-						dialog.dismiss();
-
-						//closing application
-						//finish();
-						//System.exit(0);					
-					}					
-				});
+                if (android.os.Build.VERSION.SDK_INT >= 17) {
+                    builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                        @Override
+                        public void onDismiss(DialogInterface dialog) {
+                            dialog.dismiss();
+                        }
+                    });
+                } else {
+                    builder.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                        @Override
+                        public void onCancel(DialogInterface dialog) {
+                            dialog.dismiss();
+                        }
+                    });
+                }
 				builder.show();
 			}			
 		}
@@ -129,21 +140,100 @@ public class MainActivity extends Activity {
 			builder.setTitle("Bluetooth LE not available");			
 			builder.setMessage("Sorry, this device does not support Bluetooth LE.");
 			builder.setPositiveButton(android.R.string.ok, null);
-			builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
-
-				@Override
-				public void onDismiss(DialogInterface dialog) {
-					dialog.dismiss();
-
-					//closing application
-					//finish();
-					//System.exit(0);
-				}
-
-			});
+            if (android.os.Build.VERSION.SDK_INT >= 17) {
+                builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                    @Override
+                    public void onDismiss(DialogInterface dialog) {
+                        dialog.dismiss();
+                    }
+                });
+            } else {
+                builder.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                    @Override
+                    public void onCancel(DialogInterface dialog) {
+                        dialog.dismiss();
+                    }
+                });
+            }
 			builder.show();
 
 		}
 
 	}
+
+
+    /**
+     * Verify that Google Play services is available before making a request.
+     *
+     * @return true if Google Play services is available, otherwise false
+     */
+    private boolean googlePlayServicesConnected() {
+
+        // Check that Google Play services is available
+        int resultCode =
+                GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+
+        // If Google Play services is available
+        if (ConnectionResult.SUCCESS == resultCode) {
+
+            // In debug mode, log the status
+            Log.d(TAG, "Google Play services available");
+
+            // Continue
+            return true;
+
+            // Google Play services was not available for some reason
+        } else {
+
+            // Display an error dialog
+            Dialog dialog = GooglePlayServicesUtil.getErrorDialog(resultCode, this, 0);
+            if (dialog != null) {
+                if (android.os.Build.VERSION.SDK_INT >= 11) {
+                    ErrorDialogFragment errorFragment = new ErrorDialogFragment();
+                    errorFragment.setDialog(dialog);
+                    errorFragment.show(getFragmentManager(), TAG);
+                } else {
+                    dialog.show();
+                }
+            }
+            return false;
+        }
+    }
+
+    /**
+     * Define a DialogFragment to display the error dialog generated in
+     * showErrorDialog.
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    public static class ErrorDialogFragment extends DialogFragment {
+
+        // Global field to contain the error dialog
+        private Dialog mDialog;
+
+        /**
+         * Default constructor. Sets the dialog field to null
+         */
+        public ErrorDialogFragment() {
+            super();
+            mDialog = null;
+        }
+
+        /**
+         * Set the dialog to display
+         *
+         * @param dialog An error dialog
+         */
+        public void setDialog(Dialog dialog) {
+            mDialog = dialog;
+        }
+
+        /*
+         * This method must return a Dialog to the DialogFragment.
+         */
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            return mDialog;
+        }
+    }
+
 }
--- app/src/main/java/com/radiusnetworks/campaignkitreference/MyApplication.java
+++ app/src/main/java/com/radiusnetworks/campaignkitreference/MyApplication.java
@@ -2,11 +2,17 @@ package com.radiusnetworks.campaignkitreference;
 
 import android.app.AlertDialog;
 import android.app.Application;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
 import android.util.Log;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.radiusnetworks.campaignkitreference.MainActivity;
 import com.radiusnetworks.campaignkitreference.R;
 import com.radiusnetworks.campaignkit.Campaign;
@@ -23,152 +29,150 @@ import java.util.ArrayList;
  * The <code>Application</code> class for the CampaignKit's Demo Client. It is ideal
  * to place interactions with the Campaign Kit here, due to the accessibility of the 
  * <code>Application</code> class from any Activity.
- * 
+ *
  * This class implements three required methods for the <code>CampaignKitNotifier, didFindCampaign, didSync</code>
  * and <code>didFailSync</code>. The <code>CampaignKitManager</code> constructor is called within this class's
  * onCreate method, as is necessary for <code>CampaignKit</code> functionality.
- * 
+ *
  * @author Matt Tyler
  */
 public class MyApplication extends Application implements CampaignKitNotifier {
-	public static final String TAG = "MyApplication";
+    public static final String TAG = "MyApplication";
 
-	public static ArrayList<Campaign> triggeredCampaignArray = new ArrayList<Campaign>(); //all campaigns with their beacon within range, in order of appearance
-	public static ArrayList<String> triggeredCampaignTitles = new ArrayList<String>(); //titles of all campaigns with their beacon within range, in same order
-	public CampaignKitManager _ckManager;
-	private MainActivity _mainActivity = null;
-
-	
-	@Override
-	public void onCreate() {
-		super.onCreate();
-
-		_ckManager = CampaignKitManager.getInstanceForApplication(this);
-
-		try{
-			_ckManager.enableGeofences();
-			Log.i(TAG,"onCreate. geofences enabled.");
-		}catch(GooglePlayServicesException gpse){
-			gpse.printStackTrace();
-		}catch(Exception e){
-			e.printStackTrace();
-		}
-		_ckManager.setNotifier(this);
-
-		_ckManager.start();
-
-	}
-
-	@Override
-	public void didFindCampaign(Campaign campaign) {
-		//adding Campaign to triggeredCampaignArray, this will force it to be shown on the triggeredCampaignList
-		triggeredCampaignArray.add(campaign);
-		//triggeredCampaignArray.clear();
-		//triggeredCampaignArray = _ckManager.getAllCampaigns();
-
-		//sending notification or alert, depending on whether app is in background or foreground
-		new CampaignNotificationBuilder(_mainActivity, campaign)
-		.setSmallIcon(R.drawable.ic_launcher)
-		.setOnClickActivity(DetailActivity.class)
-		.show();
-		//refreshing the visible list of campaigns
-		refreshMainActivityList();
-	}
+    public static ArrayList<Campaign> triggeredCampaignArray = new ArrayList<Campaign>(); //all campaigns with their beacon within range, in order of appearance
+    public static ArrayList<String> triggeredCampaignTitles = new ArrayList<String>(); //titles of all campaigns with their beacon within range, in same order
+    public CampaignKitManager _ckManager;
+    private MainActivity _mainActivity = null;
 
 
-	@Override
-	public void didSync() {
-		Log.i(TAG,"didSync.");
-	}
-	
-	@Override
-	public void didFailSync(Exception e) {
-		Log.e(TAG,"didFailSync.");
-		if (e != null)
-			e.printStackTrace();
-		
-	}
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        _ckManager = CampaignKitManager.getInstanceForApplication(this);
 
-	@Override
-	public void didDetectPlace(Place place, CKEventType event) {
-		Log.i(TAG,"didDetectPlace.  EventType: "+event.toString()+"  Place: "+place.toString());
+        try {
+            _ckManager.enableGeofences();
+        } catch (GooglePlayServicesException gpse) {
+            gpse.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        _ckManager.setNotifier(this);
 
-		
-	}
-	
-	public void setMainActivity(MainActivity _mainActivity) {
-		this._mainActivity = _mainActivity;
-	}
+        _ckManager.start();
 
-	/**
-	 * Refreshes triggeredCampaignTitles <code>Arraylist</code> and returns it.
-	 * @return refreshed triggeredCampaignTitles <code>Arraylist</code>.
-	 */
-	public ArrayList<String> getTriggeredCampaignTitlesList(){
-		if (triggeredCampaignArray != null){
-			triggeredCampaignTitles.clear();
-			for (Campaign c : triggeredCampaignArray){
-				triggeredCampaignTitles.add(c.getTitle());
-			}
-		}
-		return triggeredCampaignTitles;
-	}
-	
-	public ArrayList<Campaign> getTriggeredCampaignArray(){
-		return triggeredCampaignArray;
-	}
-	
-	public Campaign getCampaignFromList(int positionOnList){
-		if (triggeredCampaignArray != null  &&  triggeredCampaignArray.size() > positionOnList){
-			return triggeredCampaignArray.get(positionOnList);
-		}
-		return null;
-	}
+    }
 
-	/**
-	 * Refreshes <code>Listview</code> on the MainActivity to properly display 
-	 * campaigns associated with newly triggered beacons.
-	 */
-	private void refreshMainActivityList(){
-		if (_mainActivity != null) {
-			_mainActivity.refreshVisibleList();			
-		}
-		else {
-			Log.d(TAG, "Main activity not started yet.");
-		}
-	}
-	
-	public void removeCampaign(int position){
-		_ckManager.removeCampaign(getCampaignFromList(position));
-		triggeredCampaignArray.clear();
-		triggeredCampaignArray = _ckManager.getAllCampaigns();
-		Log.d(TAG,"after removing. triggeredCampaignArray size = "+triggeredCampaignArray.size());
+    @Override
+    public void didFindCampaign(Campaign campaign) {
+        //adding Campaign to triggeredCampaignArray, this will force it to be shown on the triggeredCampaignList
+        triggeredCampaignArray.add(campaign);
+        //triggeredCampaignArray.clear();
+        //triggeredCampaignArray = _ckManager.getAllCampaigns();
 
-		refreshMainActivityList();
-	}
-	
+        //sending notification or alert, depending on whether app is in background or foreground
+        new CampaignNotificationBuilder(_mainActivity, campaign)
+                .setSmallIcon(R.drawable.ic_launcher)
+                .setOnClickActivity(DetailActivity.class)
+                .show();
+        //refreshing the visible list of campaigns
+        refreshMainActivityList();
+    }
+
+
+    @Override
+    public void didSync() {
+        Log.i(TAG,"didSync.");
+    }
+
+    @Override
+    public void didFailSync(Exception e) {
+        Log.e(TAG,"didFailSync.");
+        if (e != null)
+            e.printStackTrace();
+
+    }
+
+    @Override
+    public void didDetectPlace(Place place, CKEventType event) {
+        Log.i(TAG,"didDetectPlace.  EventType: "+event.toString()+"  Place: "+place.toString());
+
+
+    }
+
+    public void setMainActivity(MainActivity _mainActivity) {
+        this._mainActivity = _mainActivity;
+    }
+
+    /**
+     * Refreshes triggeredCampaignTitles <code>Arraylist</code> and returns it.
+     * @return refreshed triggeredCampaignTitles <code>Arraylist</code>.
+     */
+    public ArrayList<String> getTriggeredCampaignTitlesList(){
+        if (triggeredCampaignArray != null){
+            triggeredCampaignTitles.clear();
+            for (Campaign c : triggeredCampaignArray){
+                triggeredCampaignTitles.add(c.getTitle());
+            }
+        }
+        return triggeredCampaignTitles;
+    }
+
+    public ArrayList<Campaign> getTriggeredCampaignArray(){
+        return triggeredCampaignArray;
+    }
+
+    public Campaign getCampaignFromList(int positionOnList){
+        if (triggeredCampaignArray != null  &&  triggeredCampaignArray.size() > positionOnList){
+            return triggeredCampaignArray.get(positionOnList);
+        }
+        return null;
+    }
+
+    /**
+     * Refreshes <code>Listview</code> on the MainActivity to properly display
+     * campaigns associated with newly triggered beacons.
+     */
+    private void refreshMainActivityList(){
+        if (_mainActivity != null) {
+            _mainActivity.refreshVisibleList();
+        }
+        else {
+            Log.d(TAG, "Main activity not started yet.");
+        }
+    }
+
+    public void removeCampaign(int position){
+        _ckManager.removeCampaign(getCampaignFromList(position));
+        triggeredCampaignArray.clear();
+        triggeredCampaignArray = _ckManager.getAllCampaigns();
+        Log.d(TAG,"after removing. triggeredCampaignArray size = "+triggeredCampaignArray.size());
+
+        refreshMainActivityList();
+    }
+
 
     /**
      * Records an analytics event that will be aggregated and reported within the Campaign Kit.
      *
      */
     public void recordAnalytics(CampaignKitNotifier.CKAnalyticsType type, Campaign campaign, Place place){
-    	_ckManager.recordAnalytics(type, campaign, place);
+        _ckManager.recordAnalytics(type, campaign, place);
     }
     public void recordAnalytics(CampaignKitNotifier.CKAnalyticsType type, String campaignId , String placeId){
-    	_ckManager.recordAnalytics(type, campaignId, placeId);
+        _ckManager.recordAnalytics(type, campaignId, placeId);
     }
     public void recordAnalytics(CampaignKitNotifier.CKAnalyticsType type, int campaignIdAsInt, int placeIdAsInt){
-    	_ckManager.recordAnalytics(type, campaignIdAsInt, placeIdAsInt);
+        _ckManager.recordAnalytics(type, campaignIdAsInt, placeIdAsInt);
     }
     public void recordAnalytics(CampaignKitNotifier.CKAnalyticsType type, Campaign campaign){
-    	_ckManager.recordAnalytics(type, campaign);
+        _ckManager.recordAnalytics(type, campaign);
     }
     public void recordAnalytics(CampaignKitNotifier.CKAnalyticsType type, String campaignId){
-    	_ckManager.recordAnalytics(type, campaignId);
+        _ckManager.recordAnalytics(type, campaignId);
     }
     public void recordAnalytics(CampaignKitNotifier.CKAnalyticsType type, int campaignIdAsInt){
-    	_ckManager.recordAnalytics(type, campaignIdAsInt);
+        _ckManager.recordAnalytics(type, campaignIdAsInt);
     }
 
 }
