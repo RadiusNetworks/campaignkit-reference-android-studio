9b08134b05f76ed14b1160a9e341cb650cd78a35
Aaron Kromer <aaron.kromer@gmail.com>
2015-11-17 10:03:52 -0500

    Whitespace only update.

--- app/src/main/java/com/radiusnetworks/campaignkitreference/MainActivity.java
+++ app/src/main/java/com/radiusnetworks/campaignkitreference/MainActivity.java
@@ -24,93 +24,93 @@ import java.util.Map;
 
 /**
  * The Main <code>Activity</code> for the CampaignKit's Demo Client.
- * 
+ *
  * <p>
  * A <code>ListFragment</code> is utilized in this class to display campaigns sent in from the
  * CampaignKitManager.getFoundCampaigns() method.
- * 
- * 
- * 
+ *
+ *
+ *
  * @author Matt Tyler
  *
  */
 public class MainActivity extends Activity {
-	public static final String TAG = "MainActivity";
-	Map<String,TableRow> rowMap = new HashMap<String,TableRow>();
+    public static final String TAG = "MainActivity";
+    Map<String,TableRow> rowMap = new HashMap<String,TableRow>();
 
-	private boolean _visible = false;
-	private static MyApplication _application;
-	private Context _context;
+    private boolean _visible = false;
+    private static MyApplication _application;
+    private Context _context;
 
-	public static ArrayAdapter<String> listAdapter;
+    public static ArrayAdapter<String> listAdapter;
 
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-		if (_application == null){
-			Log.d(TAG,"_application was null. initializing _application value");
-			_application = (MyApplication) this.getApplication();
-		}
-		_application.setMainActivity(this);
-		_context = this;
-		setContentView(R.layout.activity_main);
+        if (_application == null){
+            Log.d(TAG,"_application was null. initializing _application value");
+            _application = (MyApplication) this.getApplication();
+        }
+        _application.setMainActivity(this);
+        _context = this;
+        setContentView(R.layout.activity_main);
 
         verifyBluetooth();
         googlePlayServicesConnected();
-		
-		findViewById(R.id.campaignsButton).setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
 
-				//Sending to DetailActivity
-				Intent intent = new Intent();
-				intent.setClass(_context, DetailActivity.class);
-				startActivity(intent);
-				
-			}
-		});
-		findViewById(R.id.campaignsButton).setVisibility((areCampaignsTriggeredNow())? View.VISIBLE : View.GONE);
+        findViewById(R.id.campaignsButton).setOnClickListener(new OnClickListener() {
 
-	}
+            @Override
+            public void onClick(View v) {
 
-	/**
-	 * Refreshes <code>Listview</code> with current campaign titles.
-	 */
-	public void refreshVisibleList() {
-		runOnUiThread(new Runnable() {
-			public void run() {
-				findViewById(R.id.campaignsButton).setVisibility((areCampaignsTriggeredNow())? View.VISIBLE : View.GONE);
-			}
-		});
-	}
+                //Sending to DetailActivity
+                Intent intent = new Intent();
+                intent.setClass(_context, DetailActivity.class);
+                startActivity(intent);
 
-	private boolean areCampaignsTriggeredNow(){
-		if (_application == null){
-			Log.d(TAG,"_application was null. initializing _application value");
-			_application = (MyApplication) this.getApplication();
-			_application.setMainActivity(this);
-		}
-		
-		if (_application.getTriggeredCampaignArray() != null && _application.getTriggeredCampaignArray().size() >0){
-			Log.d(TAG,"_application.getTriggeredCampaignArray().size() = "+_application.getTriggeredCampaignArray().size());
-			return true;
-		}
-		
-		return false;
-	}
-	
-	private void verifyBluetooth() {
+            }
+        });
+        findViewById(R.id.campaignsButton).setVisibility((areCampaignsTriggeredNow())? View.VISIBLE : View.GONE);
 
-		try {
-			if (!org.altbeacon.beacon.BeaconManager.getInstanceForApplication(this).checkAvailability()) {
-				Log.e(TAG,"Bluetooth not enabled.");
-				final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-				builder.setTitle("Bluetooth not enabled");			
-				builder.setMessage("Please enable bluetooth in settings and restart this application.");
-				builder.setPositiveButton(android.R.string.ok, null);
+    }
+
+    /**
+     * Refreshes <code>Listview</code> with current campaign titles.
+     */
+    public void refreshVisibleList() {
+        runOnUiThread(new Runnable() {
+            public void run() {
+                findViewById(R.id.campaignsButton).setVisibility((areCampaignsTriggeredNow())? View.VISIBLE : View.GONE);
+            }
+        });
+    }
+
+    private boolean areCampaignsTriggeredNow(){
+        if (_application == null){
+            Log.d(TAG,"_application was null. initializing _application value");
+            _application = (MyApplication) this.getApplication();
+            _application.setMainActivity(this);
+        }
+
+        if (_application.getTriggeredCampaignArray() != null && _application.getTriggeredCampaignArray().size() >0){
+            Log.d(TAG,"_application.getTriggeredCampaignArray().size() = "+_application.getTriggeredCampaignArray().size());
+            return true;
+        }
+
+        return false;
+    }
+
+    private void verifyBluetooth() {
+
+        try {
+            if (!org.altbeacon.beacon.BeaconManager.getInstanceForApplication(this).checkAvailability()) {
+                Log.e(TAG,"Bluetooth not enabled.");
+                final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("Bluetooth not enabled");
+                builder.setMessage("Please enable bluetooth in settings and restart this application.");
+                builder.setPositiveButton(android.R.string.ok, null);
                 if (android.os.Build.VERSION.SDK_INT >= 17) {
                     builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
                         @Override
@@ -126,15 +126,15 @@ public class MainActivity extends Activity {
                         }
                     });
                 }
-				builder.show();
-			}			
-		}
-		catch (RuntimeException e) {
-			Log.e(TAG,"Bluetooth LE not available.");
-			final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-			builder.setTitle("Bluetooth LE not available");			
-			builder.setMessage("Sorry, this device does not support Bluetooth LE.");
-			builder.setPositiveButton(android.R.string.ok, null);
+                builder.show();
+            }
+        }
+        catch (RuntimeException e) {
+            Log.e(TAG,"Bluetooth LE not available.");
+            final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Bluetooth LE not available");
+            builder.setMessage("Sorry, this device does not support Bluetooth LE.");
+            builder.setPositiveButton(android.R.string.ok, null);
             if (android.os.Build.VERSION.SDK_INT >= 17) {
                 builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
                     @Override
@@ -150,11 +150,11 @@ public class MainActivity extends Activity {
                     }
                 });
             }
-			builder.show();
+            builder.show();
 
-		}
+        }
 
-	}
+    }
 
 
     /**
