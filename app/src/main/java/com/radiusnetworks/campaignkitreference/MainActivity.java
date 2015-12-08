00d6cfec824da3bc40960b8beffd9eab6cef68e9
Aaron Kromer <aaron.kromer@gmail.com>
2015-12-08 18:33:11 -0500

    Request permissions for Android 6
    This implements the basic flow for requesting permissions in Android 6.
    
    The only permission we need to request is access to location services.
    This is necessary so we can detect beacons in the background - via
    `ACCESS_COARSE_LOCATION`. Additionally, Google Play Service needs access
    to `ACCESS_FINE_LOCATION` for geofence triggering.
    
    This follows the flow from the Google Android tutorials. For more
    details see:
    
    - https://developer.android.com/training/permissions/index.html
    - https://www.google.com/design/spec/patterns/permissions.html
    - https://www.youtube.com/watch?v=C8lUdPVSzDk
    - https://www.youtube.com/watch?v=iZqDdvhTZj0
    
    Since the permissions we need are critical. We ask upfront. As this is a
    sample app it may, or may not, be clear why we need the permissions.
    Since this is only a reference we are no providing any on boarding and
    defer the explanation until necessary.
    
    However, because the permissions we require are critical we move the
    check from `onCreate` to `onStart`. This way, whenever the user
    re-foregrounds the app we verify that we still have the necessary
    permissions.

--- app/src/main/java/com/radiusnetworks/campaignkitreference/MainActivity.java
+++ app/src/main/java/com/radiusnetworks/campaignkitreference/MainActivity.java
@@ -1,15 +1,21 @@
 package com.radiusnetworks.campaignkitreference;
 
-import android.app.Activity;
+import android.Manifest;
+import android.annotation.TargetApi;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
@@ -25,6 +31,11 @@ public class MainActivity extends Activity {
     public static final String TAG = "MainActivity";
 
     /**
+     * Request code to use when launching the location permission resolution activity
+     */
+    private static final int REQUEST_LOCATION_ACCESS = 1;
+
+    /**
      * Store access to the button which displays the triggered Campaigns
      */
     private Button campaignsButton;
@@ -35,9 +46,6 @@ public class MainActivity extends Activity {
 
         ((MyApplication) getApplication()).setMainActivity(this);
         setContentView(R.layout.activity_main);
-
-        verifyBluetooth();
-        googlePlayServicesConnected();
 
         final Context that = this;
         campaignsButton = (Button) findViewById(R.id.campaignsButton);
@@ -52,6 +60,47 @@ public class MainActivity extends Activity {
                 }
         );
         updateCampaignsVisibility();
+    }
+
+    /**
+     * Verify critical permissions and settings every time the app is brought to the foreground.
+     */
+    @Override
+    protected void onStart() {
+        super.onStart();
+        verifyBluetooth();
+        googlePlayServicesConnected();
+        togglePermissionFeatures();
+    }
+
+    /**
+     * Called after the user has either denyed or granted our permission request.
+     *
+     * @see #togglePermissionFeatures()
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_LOCATION_ACCESS:
+                // Received permission result for location access
+
+                // Check if the only required permission has been granted
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Location access has been granted, geofences can be enabled
+                } else {
+                    // Location access was denied, so we cannot enable geofences
+                    Toast.makeText(
+                            this,
+                            "Both background beacon detection and geofence events are prevented.",
+                            Toast.LENGTH_SHORT
+                    ).show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
     }
 
     /**
@@ -71,6 +120,64 @@ public class MainActivity extends Activity {
     private boolean areCampaignsTriggeredNow() {
         MyApplication app = (MyApplication) getApplication();
         return !app.getTriggeredCampaignArray().isEmpty();
+    }
+
+    /**
+     * The basic flow for requesting permissions in Android 6.
+     * <p/>
+     * The only permission we need to request is access to location services. This is necessary
+     * so we can detect beacons in the background - via {@link
+     * android.Manifest.permission#ACCESS_COARSE_LOCATION ACCESS_COARSE_LOCATION}. Additionally,
+     * Google Play Service needs access to {@link
+     * android.Manifest.permission#ACCESS_FINE_LOCATION ACCESS_FINE_LOCATION} for geofence
+     * triggering.
+     * <p/>
+     * Since the permissions we need are critical. We ask upfront. As this is a sample app it
+     * may, or may not, be clear why we need the permissions. Since this is only a reference we
+     * are no providing any on boarding and defer the explanation until necessary.
+     *
+     * @see <a href="https://developer.android.com/training/permissions/index.html">Working with System Permissions</a>
+     * @see <a href="https://www.google.com/design/spec/patterns/permissions.html">Patterns– Permissions</a>
+     * @see <a href="https://www.youtube.com/watch?v=C8lUdPVSzDk">Runtime Permissions in Android 6.0 Marshmallow (Android Development Patterns Ep 3)</a>
+     * @see <a href="https://www.youtube.com/watch?v=iZqDdvhTZj0">Android Marshmallow 6.0: Asking For Permission</a>
+     */
+    @TargetApi(Build.VERSION_CODES.M)
+    private void togglePermissionFeatures() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            // All permissions requested at install time
+            return;
+        }
+
+        // We may already have permission so we need to check
+        // If you are not using geofences you should request ACCESS_COARSE_LOCATION:
+        // String permission = Manifest.permission.ACCESS_COARSE_LOCATION;
+        String permission = Manifest.permission.ACCESS_FINE_LOCATION;
+        if (checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED) {
+            // Location permission has already been granted
+            return;
+        }
+
+        // Location access has not been granted
+
+        /*
+         * Check if we should provide an additional rationale to the user if the permission was not
+         * granted and the user would benefit from additional context for the use of the permission.
+         *
+         * Will return `false` if the permission is disabled on the device or if the user has
+         * checked "Don't ask me again!". Will also be `false` the first time this permission
+         * is being requested. Thus this returns `true` only if we've requested the
+         * permission once before and were denied. This is a potential signal that the user
+         * might be confused about the app behavior and why this permission is necessary.
+         */
+        if (shouldShowRequestPermissionRationale(permission)) {
+            Toast.makeText(
+                    this,
+                    "Location access is needed to trigger both campaigns in the background and " +
+                            "those attached to geofences",
+                    Toast.LENGTH_LONG
+            ).show();
+        }
+        requestPermissions(new String[]{permission}, REQUEST_LOCATION_ACCESS);
     }
 
     private void updateCampaignsVisibility() {
